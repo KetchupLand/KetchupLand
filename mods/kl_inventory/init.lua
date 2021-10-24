936e15abfeec0c7c42a595ef1c72b5a9419baaff
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-10-24 19:32:47 +0200

    Text shall be black, or else it is too unreadable with Unifont.

--- mods/kl_inventory/init.lua
+++ mods/kl_inventory/init.lua
@@ -15,32 +15,32 @@ minetest.register_on_joinplayer(function(player)
 	player:get_inventory():set_size("main", 36)
 	player:hud_set_hotbar_itemcount(9)
 	local inv_formspec = [[
-		
+
 		size[10,8.5]
 		real_coordinates[true]
-		
+
 		style_type[list;spacing=0,0]
 		style[close;border=false;bgimg=kl_background.png;bgimg_pressed=kl_btn_bg_pressed.png;bgimg_middle=5,5;textcolor=#353535]
 		listcolors[#ffffff00;#ffffff80]
 		background9[0,0;0,0;kl_background.png;true;5]
-		
+
 		list[current_player;main;0.5,4.5;9,2;9]
 		list[current_player;main;0.5,7;9,1;0]
 		list[current_player;craft;4,1;3,3;]
 		list[current_player;craftpreview;8,2;1,1;]
-		
+
 		image[7,2;1,1;kl_arrow.png]
 		image[0.90,1.25;1.75,2.5;player.png]
 		box[0.10,0.10;9.35,0.4;blue]
 		box[0.5,1;2.5,3;black]
 		button_exit[9.5,0.10;0.4,0.4;close;x]
-		
+
 		label[0.25,0.30;Inventory]
 		label[0.26,0.30;Inventory]
-		style_type[label;textcolor=#353535]
+		style_type[label;textcolor=#000000]
 		label[0.5,4.25;Your inventory]
 		label[4,0.75;Crafting]
-		
+
 		${list_bg_craft}
 		${list_bg_craftpreview}
 		${list_bg_hotbar}
