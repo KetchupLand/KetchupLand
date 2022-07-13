bd63075780de95d3e203229005a168f035715527
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-07-13 23:08:39 +0200

    Fix up danil's stolen and awful snowball code

--- mods/kl_items/snowball.lua
+++ mods/kl_items/snowball.lua
@@ -1,35 +1,31 @@
+
 minetest.register_craftitem("kl_items:snowball", {
 	description = "Snowball",
-	stack_max= 16,
 	inventory_image = "kl_items_snowball.png",
-	on_use = function(itemstack, user, pointed_thing)
-        itemstack:take_item()
-		local pos = user:getpos()
-		local dir = user:get_look_dir()
-		local yaw = user:get_look_yaw()
+	on_secondary_use = function(itemstack, player)
+		itemstack:take_item()
+		local pos = player:get_pos()
+		local dir = player:get_look_dir()
 		if pos and dir then
 			pos.y = pos.y + 1.5
 			local obj = minetest.add_entity(pos, "kl_items:snowball")
 			if obj then
-				obj:setvelocity({x=dir.x * 25, y=dir.y * 25, z=dir.z * 25})
-				obj:setacceleration({x=dir.x * -3, y=-25, z=dir.z * -3})
+				obj:set_velocity({x = dir.x * 25, y = dir.y * 25, z = dir.z * 25})
+				obj:set_acceleration({x = dir.x * -3, y = -25, z = dir.z * -3})
 			end
 		end
 		return itemstack
-	end,
+	end
 })
 
 minetest.register_entity("kl_items:snowball", {
 	visual = "sprite",
-	visual_size = {x=1, y=1,},
+	visual_size = {x=1, y=1},
 	textures = {'kl_items_snowball.png'},
-	lastpos = {},
-	on_step = function(self, dtime)
-        local pos = self.object:getpos()
-        local node = minetest.get_node(pos)
-        if minetest.registered_nodes[node.name].walkable then
-            self.object:remove()
-        end
-        self.lastpos= {x = pos.x, y = pos.y, z = pos.z}
-    end
+	on_step = function(self)
+		local pos = self.object:get_pos()
+		if minetest.get_node(pos).name ~= "air" then
+			self.object:remove()
+		end
+	end
 })
