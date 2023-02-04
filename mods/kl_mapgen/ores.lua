fd436d906469301ba43a7eb3865caf5cb102cbd1
ROllerozxa <rollerozxa@voxelmanip.se>
2023-02-04 22:39:36 +0100

    Tweak ore distribution, add optimal cobalt and mese layers

--- mods/kl_mapgen/ores.lua
+++ mods/kl_mapgen/ores.lua
@@ -1,5 +1,7 @@
 --ores
 
+local stoney = {"kl_nodes:stone"}
+
 minetest.register_ore({
 	ore_type	   = "blob",
 	ore			= "kl_nodes:volcanic_dirt",
@@ -50,23 +52,12 @@ minetest.register_ore({
 })
 
 minetest.register_ore({
-	ore_type = "scatter",
-	ore = "kl_nodes:ruby_ore",
-	wherein = "kl_nodes:stone",
-	clust_scarcity = 525*3,
-	clust_num_ores = 6,
-	clust_size = 5,
-	y_min = -31000,
-	y_max = -125,
-})
-
-minetest.register_ore({
 	ore_type	   = "blob",
 	ore			= "kl_nodes:lapis",
-	wherein		= "kl_nodes:stone",
+	wherein		= stoney,
 	clust_scarcity = 525*3,
 	clust_num_ores = 50,
-	clust_size	 = 5,
+	clust_size	 = 3,
 	y_min		  = -450,
 	y_max		  = -175,
 	noise_params = {
@@ -83,10 +74,10 @@ minetest.register_ore({
 minetest.register_ore({
 	ore_type	   = "blob",
 	ore			= "kl_nodes:obsidian",
-	wherein		= "kl_nodes:stone",
+	wherein		= stoney,
 	clust_scarcity = 525*3,
 	clust_num_ores = 200,
-	clust_size	 = 25,
+	clust_size	 = 2,
 	y_min		  = -32000,
 	y_max		  = -480,
 	noise_params = {
@@ -102,88 +93,114 @@ minetest.register_ore({
 
 minetest.register_ore({
 	ore_type = "scatter",
-	ore = "kl_nodes:mese_ore",
-	wherein = "kl_nodes:stone",
+	ore = "kl_nodes:rocky_dirt",
+	wherein = "kl_nodes:dirt",
 	clust_scarcity = 525*3,
-	clust_num_ores = 6,
-	clust_size = 5,
-	y_min = -31000,
-	y_max = -100,
+	clust_num_ores = 50,
+	clust_size = 25,
+	y_min = -100,
+	y_max = 1000,
 })
 
 minetest.register_ore({
 	ore_type = "scatter",
-	ore = "kl_nodes:diamond_ore",
-	wherein = "kl_nodes:stone",
-	clust_scarcity = 525*3,
-	clust_num_ores = 6,
-	clust_size = 5,
+	ore = "kl_nodes:coal_ore",
+	wherein = stoney,
+	clust_scarcity = 9*9*9,
+	clust_num_ores = 15,
+	clust_size = 4,
 	y_min = -31000,
-	y_max = -100,
-})
-
-minetest.register_ore({
-	ore_type = "scatter",
-	ore = "kl_nodes:obsidian",
-	wherein = "kl_nodes:stone",
-	clust_scarcity = 525*3,
-	clust_num_ores = 6,
-	clust_size = 5,
-	y_min = -32000,
-	y_max = -475,
-})
-
-minetest.register_ore({
-	ore_type = "scatter",
-	ore = "kl_nodes:jade_ore",
-	wherein = "kl_nodes:stone",
-	clust_scarcity = 525*3,
-	clust_num_ores = 6,
-	clust_size = 5,
-	y_min = -31000,
-	y_max = -75,
-})
-
-minetest.register_ore({
-	ore_type = "scatter",
-	ore = "kl_nodes:cobalt_ore",
-	wherein = "kl_nodes:stone",
-	clust_scarcity = 525*3,
-	clust_num_ores = 6,
-	clust_size = 5,
-	y_min = -31000,
-	y_max = -10,
+	y_max = 100,
 })
 
 minetest.register_ore({
 	ore_type = "scatter",
 	ore = "kl_nodes:iron_ore",
-	wherein = "kl_nodes:stone",
-	clust_scarcity = 525*3,
-	clust_num_ores = 6,
-	clust_size = 5,
+	wherein = stoney,
+	clust_scarcity = 10*10*10,
+	clust_num_ores = 14,
+	clust_size = 4,
 	y_min = -31000,
 	y_max = 10,
 })
 
 minetest.register_ore({
 	ore_type = "scatter",
-	ore = "kl_nodes:rocky_dirt",
-	wherein = "kl_nodes:dirt",
-	clust_scarcity = 525*3,
-	clust_num_ores = 50,
-	clust_size = 25,
+	ore = "kl_nodes:jade_ore",
+	wherein = stoney,
+	clust_scarcity = 12*12*12,
+	clust_num_ores = 13,
+	clust_size = 4,
 	y_min = -31000,
-	y_max = 31000,
+	y_max = -150,
 })
 
 minetest.register_ore({
 	ore_type = "scatter",
-	ore = "kl_nodes:coal_ore",
-	wherein = "kl_nodes:stone",
-	clust_scarcity = 525*3,
-	clust_num_ores = 6,
-	clust_size = 5,
+	ore = "kl_nodes:cobalt_ore",
+	wherein = stoney,
+	clust_scarcity = 15*15*15,
+	clust_num_ores = 12,
+	clust_size = 4,
 	y_min = -31000,
-	y_max = 100,
+	y_max = -10,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "kl_nodes:ruby_ore",
+	wherein = stoney,
+	clust_scarcity = 17*17*17,
+	clust_num_ores = 9,
+	clust_size = 4,
+	y_min = -31000,
+	y_max = -125,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "kl_nodes:diamond_ore",
+	wherein = stoney,
+	clust_scarcity = 20*20*20,
+	clust_num_ores = 7,
+	clust_size = 4,
+	y_min = -31000,
+	y_max = -200,
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "kl_nodes:mese_ore",
+	wherein = stoney,
+	clust_scarcity = 20*20*20,
+	clust_num_ores = 5,
+	clust_size = 4,
+	y_min = -31000,
+	y_max = -300,
+})
+
+-- Random low Y value for epic farming of cobalt LOL
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "kl_nodes:cobalt_ore",
+	wherein = stoney,
+	clust_scarcity = 6*6*6,
+	clust_num_ores = 10,
+	clust_size = 3,
+	y_min = -600,
+	y_max = -500,
+})
+
+-- A sliver of mese around -1337 lol
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "kl_nodes:mese_ore",
+	wherein = stoney,
+	clust_scarcity = 6*6*6,
+	clust_num_ores = 1,
+	clust_size = 3,
+	y_min = -1337,
+	y_max = -1330,
 })
